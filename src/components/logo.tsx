373bccbcbb82441d37814304da1a039b12d4c2d9
Juan Pacheco <jfpi_0793@icloud.com>
2026-08-25 14:35:27 -0500

    fix: connect j terminal and tighten spacing

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -15,11 +15,11 @@ const Logo = ({ className, animated = false }: LogoProps) => {
       focusable="false"
     >
       <g className="logo-mark__art" transform="scale(0.64 1)">
-        <path className="logo-mark__path logo-mark__path--j" d="M 20 73 H 76 C 90 73 100 62 100 43 V 20" />
+        <path className="logo-mark__path logo-mark__path--j" d="M 34 73 H 76 C 90 73 100 62 100 43 V 20" />
         <ellipse className="logo-mark__path logo-mark__path--j-terminal" cx="20" cy="73" rx="14.06" ry="9" />
 
-        <path className="logo-mark__path logo-mark__path--f" d="M 178 73 V 30 C 178 24 186 20 198 20 H 260" />
-        <path className="logo-mark__path logo-mark__path--f-cross" d="M 193 51 H 245" />
+        <path className="logo-mark__path logo-mark__path--f" d="M 140 73 V 30 C 140 24 148 20 160 20 H 222" />
+        <path className="logo-mark__path logo-mark__path--f-cross" d="M 155 51 H 207" />
 
         <path className="logo-mark__path logo-mark__path--p" d="M 292 73 V 20 H 353 C 372 20 380 26 380 34 C 380 42 372 47 353 47 H 292" />
 
